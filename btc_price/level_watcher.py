7af36f8f5598e309f55ac24e859ecd23ba519e42
johnblakey <4926778+johnblakey@users.noreply.github.com>
2021-09-18 18:21:23 +0000

    Added comments for message sent

--- btc_price/level_watcher.py
+++ btc_price/level_watcher.py
@@ -19,17 +19,20 @@ def gas_push_calculation():
     for row in query_job:
         fees.append(row[0])
 
-    # TODO add way for user to apply limit
-    # Fee limit set to 30
-    limit = 30
+    # TODO add way for user to apply Fee alert limit (data studio?)
+    limit = 40
 
     # TODO timer of 30 minutes once a message is sent (build into sms push?)
 
     # Simple logic to send a message based on prior fee reading
     if fees[0] > limit and fees[1] <= limit:
-        send_sms("Current gas fee: {} is now higher than your setting:{}".format(row[0], limit))
-    if fees[0] < limit and fees[1] >= limit:
-        send_sms("Current gas fee: {} is now lower than your setting:{}".format(row[0], limit))
+        send_sms("Current gas fee: {} is now higher than your setting:{}".format(fees[0], limit))
+        logging.info("SMS sent: Current gas fee: {} is now higher than your setting:{}".format(fees[0], limit)) 
+    elif fees[0] < limit and fees[1] >= limit:
+        send_sms("Current gas fee: {} is now lower than your setting:{}".format(fees[0], limit))
+        logging.info("SMS sent: Current gas fee: {} is now lower than your setting:{}".format(fees[0], limit))
+    else:
+        logging.info("No SMS sent: Current gas fee: {}".format(fees[0]))
 
-    return "Query finished, current ETH Gas fee is {}".format(rows[0])
+    return "Query finished, current ETH Gas fee is {}".format(fees[0])
     
